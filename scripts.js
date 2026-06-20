2aa3a7df75b3caeab09c20027a321d9ac9cdf87c
Kamran Ali <kamranroonjha94@gmail.com>
2026-06-20 12:53:34 +0500

    Make seekbar as change duration of song

--- scripts.js
+++ scripts.js
@@ -1,40 +1,56 @@
 console.log("Lets write javascripts")
 async function getsongs() {
-    let a=await fetch("http://127.0.0.1:3000/songs/")
-    let response=await a.text()
+    let a = await fetch("http://127.0.0.1:3000/songs/")
+    let response = await a.text()
     // console.log(response)   
-    let div=document.createElement("div")
-    div.innerHTML=response;
-    let as=div.getElementsByTagName("a")
+    let div = document.createElement("div")
+    div.innerHTML = response;
+    let as = div.getElementsByTagName("a")
     // console.log(as)
-    let songs=[]
+    let songs = []
     for (let index = 0; index < as.length; index++) {
         const element = as[index];
-        if(element.href.endsWith(".mp3"))
-        {
+        if (element.href.endsWith(".mp3")) {
             songs.push(element.href.split("songs%5C")[1])
-            
+
         }
         // console.log(songs)
     }
     return songs
 }
-let currentSong=new Audio()
-const playMusic=(track)=>
-{
+let currentSong = new Audio()
+const playMusic = (track, pause=false) => {
     // let audio=new Audio("/songs/"+track)
-    currentSong.src="/songs/"+track
-    currentSong.play()
-    play.src="pause-button.svg"
-}
+    currentSong.src = "/songs/" + track
+    if (!pause) {
+        currentSong.play()
+        play.src = "pause-button.svg"
+    }
+    else{
+        play.src="play-button.svg"
+    }
+    document.querySelector(".songinfo").innerHTML = decodeURI(track)
+    document.querySelector(".songtime").innerHTML = "00:00 / 00:00"
 
+}
+function formatTime(time) {
+    let minutes = Math.floor(time / 60);
+    let seconds = Math.floor(time % 60);
+
+    // Add leading zero if seconds < 10
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    return `${minutes}:${seconds}`;
+}
 async function main() {
-    let song=await getsongs()
+    let song = await getsongs()
+   
+    playMusic(song[0],true)
     // console.log(song)
     let songUl = document.querySelector(".playlist").getElementsByTagName("ul")[0]
     for (const element of song) {
         let name = decodeURIComponent(element).trim().replaceAll("_", " ")
-        songUl.innerHTML=songUl.innerHTML + `
+        songUl.innerHTML = songUl.innerHTML + `
         <li>
                 <div class="info">
                   <div>${name}</div>
@@ -46,27 +62,36 @@ async function main() {
                 </div>
               </li>
               `
-        
+
     }
     Array.from(document.querySelector(".playlist").getElementsByTagName("li")).forEach(e => {
-        e.addEventListener("click",element=>{
+        e.addEventListener("click", element => {
             console.log(e.querySelector(".info").firstElementChild.innerHTML)
             playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim())
 
         })
-    })  
-    
-    play.addEventListener("click",()=>{
-        if(currentSong.paused)
-        {
+    })
+
+    play.addEventListener("click", () => {
+        if (currentSong.paused) {
             currentSong.play()
-            play.src="pause-button.svg"
+            play.src = "pause-button.svg"
         }
-        else
-            {
-                currentSong.pause()
-                play.src="play-button.svg"
+        else {
+            currentSong.pause()
+            play.src = "play-button.svg"
         }
+    })
+    currentSong.addEventListener("timeupdate", () => {
+        console.log(currentSong.currentTime, currentSong.duration)
+        document.querySelector(".songtime").innerHTML = `${formatTime(currentSong.currentTime)} / ${formatTime(currentSong.duration)}`
+        document.querySelector(".circle").style.left=(currentSong.currentTime/currentSong.duration)*100+"%"
+    })
+
+    document.querySelector(".seekbar").addEventListener("click",e=>{
+        let per= (e.offsetX/e.target.getBoundingClientRect().width)*100
+        document.querySelector(".circle").style.left=per+"%"
+        currentSong.currentTime=((currentSong.duration)*per)/100
     })
 }
 main()
