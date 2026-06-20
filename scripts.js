00ec34e470dea966f4d4aa9f6ec5604cac8ece13
Kamran Ali <kamranroonjha94@gmail.com>
2026-06-20 14:28:00 +0500

    Making website responsive for all deviced using media querries

--- scripts.js
+++ scripts.js
@@ -93,5 +93,11 @@ async function main() {
         document.querySelector(".circle").style.left=per+"%"
         currentSong.currentTime=((currentSong.duration)*per)/100
     })
+    document.querySelector(".hamburger").addEventListener("click",()=>{
+        document.querySelector(".left").style.left="0"
+    })
+    document.querySelector(".close").addEventListener("click",()=>{
+        document.querySelector(".left").style.left="-100%"
+    })
 }
 main()
